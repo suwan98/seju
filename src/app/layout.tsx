a19051ca3850809257c20fc4e428f93eb781dfe9
Suwan <imissyou5466@gmail.com>
2023-12-05 14:10:03 +0900

    📄Docs : README 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type {Metadata} from "next";
-import {Inter} from "next/font/google";
 import "@/styles/tailwind.css";
 import ThemeProviders from "../components/ThemeProviders";
 import Navbar from "../components/Navbar";
