07652176234971249faa0ab1160529e5cb69558b
Suwan <imissyou5466@gmail.com>
2023-12-12 13:45:48 +0900

    🐛 Fix : nextConfig basePath 수정

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const {withContentlayer} = require("next-contentlayer");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: "/app",
 };
 
 module.exports = withContentlayer(nextConfig);
