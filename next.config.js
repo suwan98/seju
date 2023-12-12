5b7ff152031dd855bfb54cf80421d0ccb3d825e1
Suwan <imissyou5466@gmail.com>
2023-12-12 13:51:08 +0900

    🔧 Fix : 도메인 재설정

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const {withContentlayer} = require("next-contentlayer");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/app",
 };
 
 module.exports = withContentlayer(nextConfig);
