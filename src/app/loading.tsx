0b55ae35da01501d1db63318fe2b19e37896f9e8
Suwan <imissyou5466@gmail.com>
2023-12-14 22:59:49 +0900

    ✨ Feat : 로딩페이지 추가

---
+++ src/app/loading.tsx
@@ -0,0 +1,13 @@
+import {BeatLoader} from "react-spinners";
+
+function loading() {
+  return (
+    <>
+      <div>
+        <BeatLoader color="rgb(59 130 246)" loading />
+      </div>
+    </>
+  );
+}
+
+export default loading;
